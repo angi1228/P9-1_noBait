c98eed4c0195209cb43ba85b0074cc6f38156c4b
jinminglim <limjinming2001@gmail.com>
2025-09-13 16:42:26 +0800

    updated some dynamic variable for testing and risk score table
    dynamic variable for the number of suspicious stuff detected (just a trial code), added scoring table to classify email.

--- app.py
+++ app.py
@@ -4,10 +4,27 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def home():
-    return render_template("home.html")
+# def home():
+#     return render_template("home.html")
 
 
+def test():
+    totalemails = ['email1','email2','email3','email4','malicious']
+    totalscore = 0
+    
+    total_length = len(totalemails)
+    
+    suskeywords = ['pay','scam','malicious']
+
+    for i in totalemails:
+        if i in suskeywords:
+            score = 9
+            totalscore += score
+            reason = f"the word {i} is very suspicous thats why it is being flagged out!!"
+
+    return render_template("home.html",total_length=total_length,totalscore=totalscore,reason=reason)
+
+    
 
 
 
