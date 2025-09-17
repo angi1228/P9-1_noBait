e395b3a3f467d049dd82920abd1f9d1eefcf83ba
Angir288 <58065737+angi1228@users.noreply.github.com>
2025-09-17 23:31:58 +0800

    url_check.py function for page
    Checks url path is in suspicious keyword list
    Problem with domainscore list in check domain function, will restart to try and resolve

--- url_check.py
+++ url_check.py
@@ -39,33 +39,58 @@ def check_domain(parsed_url_list):
 
     # list for riskscore 
     domain_score = []
+    # for every url in list
     for i in parsed_url_list:
-        if i.netloc not in TRUSTED_DOMAINS:
-            print("UNTRUSTED")
-            if i.netloc in SHORTENING_SERVICES:
+        if i.netloc not in TRUSTED_DOMAINS:     # if domain name not in trusted domains
+            # print("UNTRUSTED")
+            if i.netloc in SHORTENING_SERVICES: # if domain name is url shortener
                 print("URL_SHORTENER USED")
                 riskscore += 70
                 print(riskscore)
+                domain_score.append(riskscore)  
+                break
+            else:                                  # can't seem to get urls with unknown domains into else loop | check after restart
+                print("UNTRUSTED DOMAIN")
+                riskscore += 50
+                print(riskscore)
                 domain_score.append(riskscore)
                 break
-            riskscore += 50
+        else:
+            print("Trusted")
+            print(riskscore)
             domain_score.append(riskscore)
-            break
-        print("Trusted")
-        print(riskscore)
-        domain_score.append(riskscore)
     print(domain_score)
     return domain_score
 
+def check_page(parsed_url_list, domain_score):
+    # run through url again
+    count = 0 
+    print(f'Score List: {domain_score}')
+    for i in parsed_url_list:
+        # if page name found in suspicious keywords
+        print(i.path[1:])
+        if i.path[1:] in SUSPICIOUS_KEYWORDS:       #  string sliced to remove /
+            print("SUSPICIOUS PAGE NAME")
+            print(domain_score[count])
+            domain_score[count] += 50
+            print(domain_score[count])
+            break
+        count+=1
+    print(domain_score)
+    return domain_score
+        
 
 
 
 def check_url(email_body):
     found_url_list = extract_URL(email_body)
-    parsed_list = parse_URL(['http://yahoo.com', "https://tinyurl.com/utdmmett"])
+    parsed_list = parse_URL(['http://yahoo.com', 'https://tinyurl.com/utdmmett', 'https://bankscam.net/login'])
     domainscore = check_domain(parsed_list)
+    check_page(parsed_list, domainscore)
+    #domain score is a list 
     parsed_list = parse_URL(found_url_list)
     domainscore = check_domain(parsed_list)
+    check_page(parsed_list, domainscore)
 
 
 check_url(body)
